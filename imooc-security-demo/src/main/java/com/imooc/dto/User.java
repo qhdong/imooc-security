510f80fa16ff8d9590b83d3284f449962919f3e9
Dong Qihong <dongqihong@vip.qq.com>
2017-12-01 22:48:26 +0800

    rest，第三章

---
+++ imooc-security-demo/src/main/java/com/imooc/dto/User.java
@@ -0,0 +1,11 @@
+package com.imooc.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String username;
+
+    private String password;
+}
---
+++ imooc-security-demo/src/main/java/com/imooc/web/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.imooc.web.controller;
+
+import com.imooc.dto.User;
+import com.imooc.dto.UserQueryCondition;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+    public List<User> query(UserQueryCondition condition,
+                            @PageableDefault(size = 15, page = 1, sort = "username,asc") Pageable pageable) {
+
+        System.out.println(ReflectionToStringBuilder.toString(condition, ToStringStyle.MULTI_LINE_STYLE));
+
+        System.out.println(pageable.getPageSize());
+        System.out.println(pageable.getPageNumber());
+        System.out.println(pageable.getSort());
+
+        List<User> users = new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        return users;
+    }
+
+    @GetMapping("/user/{id:\\d+}")
+    public User getInfo(@PathVariable String id) {
+        User user = new User();
+        user.setUsername("tom");
+        return user;
+    }
+}
